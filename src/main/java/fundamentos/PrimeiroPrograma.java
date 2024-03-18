b6be9b394304ba8f9ea64bd597387fa12cd43bec
Adailton Simplicio <adailtonsimplicio.s@gmail.com>
2024-03-17 23:41:25 -0300

    Initial commit

---
+++ src/main/java/fundamentos/PrimeiroPrograma.java
@@ -0,0 +1,29 @@
+/**
+ * @author adailton simplcio
+ * @since JDK1.0
+ *
+ * */
+public class PrimeiroPrograma{
+    /**
+     * ...
+     * @param args
+     */
+public static void main(String[] args){
+
+    // Uma senteça de códgo termina com ;
+    // Mais uma linha...
+    // Fim
+    System.out.println("Primeiro Programa parte #01!!");
+
+        /*
+         linha 1
+         linha 2
+        */
+
+    System.out.println("Primeiro Programa parte #02!!");
+
+    System.out.println("Fim!");
+}
+}
+
+
